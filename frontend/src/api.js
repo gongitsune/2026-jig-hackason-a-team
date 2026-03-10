ebb94030f86af30e2f4ab511f6409ef2e9cc2948
teraki <teraki@jig.jp>
2026-03-10 17:09:23 +0900

    フロントの接続先をデプロイしたサーバーに変更

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = "http://localhost:8080";
+const BACKEND_URL = "https://two026-jig-hackason-a-team.onrender.com";
 
 export const getUserId = () => {
 	const value = localStorage.getItem("userId");
