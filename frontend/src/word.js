e4cb6ff9e9faa51a515da87fb921a64ea1adb97d
g22307 <g22307@kagawa.kosen-ac.jp>
2026-03-10 15:24:37 +0900

    word.jsを修正

--- frontend/src/word.js
+++ frontend/src/word.js
@@ -1,43 +1,44 @@
 import { API } from "./api.js";
 
-var target = "世界一強い武器";
+const roomStatus = await API.getRoomStatus();
 
 addRoomStatusListener((updatedStatus) => {
-  if (updatedStatus.status === "") {
-    window.location.href = "/sentence.html";
-  }
+	if (updatedStatus.status === "SENTENCE_INPUT") {
+		window.location.href = "/sentence.html";
+	}
 });
 
-var target_text = document.getElementById("target");
-target_text.textContent = target;
+const target_text = document.getElementById("target");
+target_text.textContent = roomStatus.goal;
 
-var words= document.getElementsByClassName("card");
-var input_word= document.getElementById("word-textarea");
-var button= document.getElementById("submit-button");
-button.onclick=submit_word;
+var words = document.getElementsByClassName("card");
+var input_word = document.getElementById("word-textarea");
+var button = document.getElementById("submit-button");
+button.onclick = submit_word;
 
-function submit_word(){
-  if(input_word.value === ""){
-    alert("入力を行ってください");
-    return;
-  }
-  for(var i = 0;i < words.length;i++){
-    var child = words[i].children[0];
-    if(child.className === "preword-0"){
-      child.className = "preword-1";
-      child.textContent = input_word.value;
-      input_word.value = "";
-      if(i < words.length - 1)return;
-    }else if(child.textContent === input_word.value){
-      input_word.value = "";
-      alert("単語が被っています");
-      return;
-    }
-  }
-  submit();
-  return;
+function submit_word() {
+	if (input_word.value === "") {
+		alert("入力を行ってください");
+		return;
+	}
+	for (let i = 0; i < words.length; i++) {
+		const child = words[i].children[0];
+		if (child.className === "preword-0") {
+			child.className = "preword-1";
+			child.textContent = input_word.value;
+			input_word.value = "";
+			if (i < words.length - 1) return;
+		} else if (child.textContent === input_word.value) {
+			input_word.value = "";
+			alert("単語が被っています");
+			return;
+		}
+	}
+	submit();
+	return;
 }
 
-function submit(){
-  API.postWords(words[0].textContent);
+function submit() {
+	API.postWords(words[0].textContent);
 }
+
